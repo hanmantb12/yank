6fa6936221f824a8fda1aff5d8ac9442fd48dd83
hanmantb12 <hanmantb12@gmail.com>
2021-08-06 18:32:14 +0530

    first commit

---
+++ src/main/java/com/orenda/xpert/OrendabanklastApplication.java
@@ -0,0 +1,13 @@
+package com.orenda.xpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrendabanklastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrendabanklastApplication.class, args);
+	}
+
+}
