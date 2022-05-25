2fd036b006d2248f7431d4a537c848668c36f0fb
Hanmant Bhingole <hanmantb12@gmail.com>
2022-05-25 11:46:40 +0530

    Update OrendabanklastApplicationTests.java

--- src/test/java/com/orenda/xpert/OrendabanklastApplicationTests.java
+++ src/test/java/com/orenda/xpert/OrendabanklastApplicationTests.java
@@ -6,14 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrendabanklastApplicationTests {
 	
-	
-	
 
-
-
-	
 	@Test
 	void contextLoads() {
+		
+		System.out.println("hhhhhhhhhhh");
 	}
 
 }
